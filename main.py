0f10fdd5719670a54f2737109e3f0e8e00e817dc
Quentin.Tu <tost84226@gmail.com>
2017-09-30 12:07:12 +0800

    Build on Docker

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from models.stock.StockBaseModel import StockBaseModel
 from models.stock.StockModel import StockModel
 
 if __name__ == '__main__':
-	# StockCrawlerService.fetchAllStockBaseModelsFromInternet() #首次使用前須先執行，才可Fetch所有'標的'下來
+	StockCrawlerService.fetchAllStockBaseModelsFromInternet() #首次使用前須先執行，才可Fetch所有'標的'下來
 
-	# StockCrawlerService.fetchAllStockCategoryModelsFromInternet() #首次使用前須先執行，才可Fetch所有'類別'下來
+	StockCrawlerService.fetchAllStockCategoryModelsFromInternet() #首次使用前須先執行，才可Fetch所有'類別'下來
 
 	l = StockService.getAllStockBaseModels()
 	for model in l:
--- storages/BaseStorage.py
+++ storages/BaseStorage.py
@@ -3,6 +3,6 @@ import MySQLdb
 
 class BaseStorage(object):
 	"""docstring for BaseStorage""" #下方[host],[user],[passwd],[db],[charset] 皆需自行設定
-	db = MySQLdb.connect(host="192.168.1.5", user="finance", passwd="finance_fb3efc4", db="finance", charset="utf8")
+	db = MySQLdb.connect(host="172.17.0.2", user="root", passwd="test", db="stock", charset="utf8")
 	def __init__(self):
 		super(BaseStorage, self).__init__()
